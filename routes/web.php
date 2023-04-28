fe48c83f8a6b81e58339eaeb9c900a72b88b5c0a
khizerdev <46632721+khizerdev@users.noreply.github.com>
2023-04-29 01:07:01 +0500

    Splade Link Component for SPA

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,6 +31,8 @@ Route::middleware('splade')->group(function () {
     Route::get('/', function () {
         return view('welcome');
     });
+
+    Route::get('users' , [UserController::class, 'index'])->name('users');
 
     Route::middleware('auth')->group(function () {
         Route::get('/dashboard', function () {
